188f0f0fa1f90b7a4407c73f28223e8824136231
togga117 <59132475+togga117@users.noreply.github.com>
2020-11-16 17:37:42 -0300

    Update footer.js

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -74,23 +74,23 @@ const FooterContent = `
   <div>
     <div id="footer">
     <p class="lead">
-      3. Export the project
+      3. Exportar el proyecto
     </p>
 
     <p class="paragraph">
-        Your project is going to be safely hosted on GitHub - a space for code repositories online.
+        Su proyecto se alojará de forma segura en GitHub, un espacio para repositorios de código en línea.
     </p>
     <p class="paragraph">
-    If you would like to save your project locally, you can also download the package containing the generated code and all supporting files.
+    Si desea guardar su proyecto localmente, también puede descargar el paquete que contiene el código generado y todos los archivos de soporte.
     </p>
 
     <div class="buttons">
       <button id="github-publish" disabled class="primary-button publish-disabled">
-        Publish on Github
+        Publicar en Github
       </button>
 
       <button id="zip-publish" disabled class="passive-button publish-disabled">
-        Download package
+        Descargar paquete
       </button>
     </div>
     </div>
